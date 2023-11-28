afb19c4040e273d0e9f419b447da662993b7fb07
Myselfshiwoziji <138869684+Myselfshiwoziji@users.noreply.github.com>
2023-11-28 20:23:10 +0800

    gitignore

---
+++ scenes.js
@@ -0,0 +1,17 @@
+import Phaser from 'phaser'
+
+class Load extends Phaser.Scene {
+
+    constructor(){
+        super('load')
+    }
+
+    preload(){ 
+
+    }
+
+    create() {
+        
+    }
+
+}
