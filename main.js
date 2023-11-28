884abbf7af85e7bcbe3460680f7908791f031835
Myselfshiwoziji <138869684+Myselfshiwoziji@users.noreply.github.com>
2023-11-28 20:47:57 +0800

    1 scene

---
+++ main.js
@@ -0,0 +1,19 @@
+import Phaser from 'phaser'
+
+import Load from './scenes'
+
+const config = {
+	type: Phaser.AUTO,
+	parent: 'app',
+	width: 800,
+	height: 600,
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: { y: 200 },
+		},
+	},
+	scene: [Load],
+}
+
+export default new Phaser.Game(config)
--- scenes.js
+++ scenes.js
@@ -7,11 +7,12 @@ class Load extends Phaser.Scene {
     }
 
     preload(){ 
-
+        this.load.image('Background', 'assets/Background.png')
+        this.load.spritesheet('Stickman', 'assets/Stickman.png', {frameWidth: 32, frameHeight: 48})
     }
 
     create() {
-        
+        this.add.image('owen', 'assets/owen_jones.png')
     }
 
 }
