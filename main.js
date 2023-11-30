1afc013b694df3e1a18c792ff12b3831e02d8385
Myselfshiwoziji <138869684+Myselfshiwoziji@users.noreply.github.com>
2023-11-30 11:24:38 +0800

    player anims

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 import Phaser from 'phaser'
 
+import loading_screen from './scenes.js'
 import thing from './scenes.js'
+
 
 const config = {
 	type: Phaser.AUTO,
 	parent: 'app',
-	width: 800,
-	height: 600,
+	width: 1800,
+	height: 1000,
 	physics: {
 		default: 'arcade',
 		arcade: {
 			gravity: { y: 200 },
 		},
 	},
-	scene: [thing]
+	scene: [loading_screen]
 }
 
 export default new Phaser.Game(config)
